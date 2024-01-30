3fa1b81f8d3756c8907b68a6a9ca3d5978735476
phk422 <1769476785@qq.com>
2024-01-30 22:16:04 +0800

    chore: 初始化项目

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/pengdocs/',
+  outDir: '../dist',
+  title: 'PengDocs',
+  description: '我的个人博客',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    search: {
+      provider: 'local',
+    },
+    outline: 'deep',
+    outlineTitle: '文章目录',
+    logo: '/logo.png',
+    returnToTopLabel: '返回顶部',
+    sidebarMenuLabel: '目录',
+    darkModeSwitchLabel: '主题切换',
+    footer: {
+      message: 'Released under the <a href="https://github.com/phk422/phkdocs/blob/master/LICENSE" target="_blank">MIT License</a>.',
+      copyright: 'Copyright © 2023-present <a href="https://github.com/phk422" target="_blank">phk422</a>.',
+    },
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/phk422' },
+      { icon: 'npm', link: 'https://www.npmjs.com/~codervae' },
+    ],
+  },
+
+  head: [
+    ['link', { rel: 'icon', type: 'image/png', href: '/pengdocs/logo.png' }],
+  ],
+})
---
+++ docs/index.md
@@ -0,0 +1,27 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: PengDocs
+  text: Peng's Personal Blog
+  tagline: My Learning Collection
+  image:
+    src: /logo.png
+    alt: PengDocs
+  actions:
+    - theme: brand
+      text: Start
+      link: /markdown-examples
+    - theme: alt
+      text: About Me
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
